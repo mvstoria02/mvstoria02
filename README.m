0f48d8139b451b4ecd17571e4ef7578f85af44fb
mvstoria02 <141172777+mvstoria02@users.noreply.github.com>
2023-08-01 14:55:32 -0300

    my profile

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @mvstoria02
+- 👀 I’m interested in movies
+- 🌱 I’m currently studying direito
+- 💞️ I’m looking to collaborate on nothing 
+- 📫 How to reach me ... instagram : @mvstoria
+
+<!---
+mvstoria02/mvstoria02 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->

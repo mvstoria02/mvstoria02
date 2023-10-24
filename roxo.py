c56354f92c52553e0f905fd96e558e6e67788d38
mvstoria02 <141172777+mvstoria02@users.noreply.github.com>
2023-10-24 14:35:13 -0300

    Add files via upload

---
+++ roxo.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def simples (cor):
+    if cor == 'roxo':
+      return 'escolheu certo'
+  
+def medio (cor):
+    if cor == 'lilas':
+        return 'escolheu certo'
+    else:
+        return 'tente outra cor'
+    
+def completo (cor):
+    if cor == 'laranja':
+        return 'escolheu certo'
+    elif cor == 'preto':
+        return 'não tem salvação'
+    else:
+        return 'tente outra cor'
